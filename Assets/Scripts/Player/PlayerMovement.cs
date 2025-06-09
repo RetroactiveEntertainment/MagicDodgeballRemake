c175eaec49c3b9bdeba08aa990655f7271491052
Mert YILMAZ <mertyilmaz.dev@gmail.com>
2025-06-09 16:52:23 +0200

    Code cleanup

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -16,42 +16,13 @@ public class PlayerMovement : MonoBehaviour
         _moveInput = value.Get<Vector2>();
     }
 
-    public void OnLook(InputValue value)
-    {
-        _lookInput = value.Get<Vector2>();
-        //Debug.Log(_lookInput);
-    }
-
     private void FixedUpdate()
     {
         HandleMovement();
-        //HandleLookInstant();
     }
 
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector3(-_moveInput.y, 0f, _moveInput.x).normalized * baseSpeed;
-    }
-
-    private void HandleLookInstant()
-    {
-        if (_lookInput.sqrMagnitude < 0.1f)
-            return;
-
-        Vector3 lookDir = Vector3.left * _lookInput.y + Vector3.forward * _lookInput.x;
-
-        Quaternion newRotation = Quaternion.LookRotation(lookDir, Vector3.up);
-        rb.MoveRotation(newRotation);
-    }
-
-    private void HandleLookSmooth()
-    {
-        if (_lookInput.sqrMagnitude < 0.1f)
-            return;
-
-        Vector3 lookDir = Vector3.left * _lookInput.y + Vector3.forward * _lookInput.x;
-
-        Quaternion newRotation = Quaternion.LookRotation(lookDir, Vector3.up);
-        rb.MoveRotation(Quaternion.RotateTowards(rb.rotation, newRotation, rotationSpeed));
     }
 }
