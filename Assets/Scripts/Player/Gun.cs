a634b7c89f537ca933ee834b2c49561f304331b7
Mert YILMAZ <mertyilmaz.dev@gmail.com>
2025-06-22 16:14:12 +0200

    Reworked Gun
    Merged the Gun script and the Projectile Shooter as it was unnecessary to abstract them in the first place.

--- Assets/Scripts/Player/Gun.cs
+++ Assets/Scripts/Player/Gun.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,20 +9,67 @@ public class Gun : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private Transform projectileSpawnPoint;
     [SerializeField] private ProjectileData modifiedProjectileData;
-    private ProjectileSpawner _projectileSpawner = new ProjectileSpawner(3);
+
+    private GameObject _cachedProjectile = null;
+    //private ProjectileSpawner _projectileSpawner = new ProjectileSpawner(3);
+
+    private int _maxAllowedAliveProjectiles = 3;
+    private List<Projectile> _projectiles = new List<Projectile>();
 
     private void Start()
     {
-        _projectileSpawner.SetPlayerIndex(playerIndex);
+        CreateCachedProjectile();
+    }
+
+
+    // Creates a projectile that can be used to implement buffs in. All projectiles are based off this cached version.
+    private void CreateCachedProjectile()
+    {
+        _cachedProjectile = Instantiate(projectilePrefab);
+        _cachedProjectile.SetActive(false);
     }
 
     public void OnShoot(InputValue value)
     {
-        _projectileSpawner.SpawnNewProjectile(projectilePrefab, modifiedProjectileData, projectileSpawnPoint);
+        SpawnNewProjectile(_cachedProjectile, modifiedProjectileData);
+    }
+
+    private bool CanSpawnNewProjectile()
+    {
+        return _projectiles.Count < _maxAllowedAliveProjectiles;
+    }
+
+    private void RemoveProjectileFromAliveList(Projectile projectile)
+    {
+        // Check if it exists, edge case!
+        if (_projectiles.Contains(projectile))
+            _projectiles.Remove(projectile);
+    }
+
+    public int GetMaxAllowedAliveProjectiles()
+    {
+        return _maxAllowedAliveProjectiles;
     }
 
     public void SetMaxAllowedAliveProjectiles(int maxAllowedAliveProjectiles)
     {
-        _projectileSpawner.SetMaxAllowedAliveProjectiles(maxAllowedAliveProjectiles);
+        _maxAllowedAliveProjectiles = maxAllowedAliveProjectiles;
+    }
+
+    private void SpawnNewProjectile(GameObject projectileToSpawn, ProjectileData projectileData)
+    {
+        if (!CanSpawnNewProjectile())
+            return;
+
+        GameObject projectileGameObject = Instantiate(projectileToSpawn, projectileSpawnPoint.position,
+            projectileSpawnPoint.rotation);
+        Projectile projectile = projectileGameObject.GetComponent<Projectile>();
+        projectile.SetProjectileData(projectileData);
+        projectile.OnProjectileDeath += RemoveProjectileFromAliveList;
+        _projectiles.Add(projectile);
+        projectile.SpawnedByPlayerIndex = playerIndex;
+
+        projectile.gameObject.SetActive(true);
+        //Debug.Log($"Projectile count: {_projectiles.Count}");
     }
 }
